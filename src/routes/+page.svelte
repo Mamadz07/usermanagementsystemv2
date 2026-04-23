b33593105e009b58f8caa2e270b839c760367fac
Mamadz07 <muhammadilham3025@gmail.com>
2026-04-23 20:51:46 +0700

    creat-user

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
-<form method="POST" style="display:flex; margin: auto;">
-    <input name="username" placeholder="Username" required />
-    <input type="email" placeholder="Email" required />
-    <input name="alamat" placeholder="Alamat" />
+<form method="POST" style="display:flexbox; margin: auto; margin-top: 20px; margin-left: 10px; background-color: aqua; padding-left: 20px;">
+<label>Create User</label>
+<label style="display: block;">Nama</label>
+    <input name="username" placeholder="Username" required  style="display:flex;/">
+    <label style="display: block;">Email</label>
+    <input type="email" placeholder="Email" required style="display: flex;"/>
+    <label style="display: block;">Alamat</label>
+    <input name="alamat" placeholder="Alamat" style="display: flex;" />
     <input type="file" placeholder="Foto Profil" />
-    <button type="submit">Create</button>
+    <button type="submit" style="display: flex; background-color: blue;">Create</button>
 </form>
