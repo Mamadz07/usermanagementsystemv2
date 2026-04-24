9af09f75fa7764eb5c5f31696b711e5bde2938b4
Mamadz07 <muhammadilham3025@gmail.com>
2026-04-24 09:04:15 +0700

    ginidulu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,16 @@
-<form method="POST" style="display:flexbox; margin: auto; margin-top: 20px; margin-left: 10px; background-color: aqua; padding-left: 20px;">
-<label>Create User</label>
-<label style="display: block;">Nama</label>
-    <input name="username" placeholder="Username" required  style="display:flex;/">
-    <label style="display: block;">Email</label>
-    <input type="email" placeholder="Email" required style="display: flex;"/>
-    <label style="display: block;">Alamat</label>
-    <input name="alamat" placeholder="Alamat" style="display: flex;" />
-    <input type="file" placeholder="Foto Profil" />
-    <button type="submit" style="display: flex; background-color: blue;">Create</button>
+<form method="POST" style="display:inline-block; margin: auto; margin-top: 20px; margin-left: 10px; background-color: aqua; padding-left: 20px; justify-content:end; box-shadow: 1px 1px 20px 1px;">
+<label style="display: flex; justify-content: center; padding-top: 20px;">Create User</label>
+
+    <label style="display: block; padding-top: 10px;">Nama</label>
+    <input name="username" placeholder="Username" required  style="display:flex; padding-top: 10px; padding-right: 80px;/">
+
+    <label style="display: block; padding-top: 10px;">Email</label>   
+    <input type="email" placeholder="Email" required style="display: flex; justify-content: center; padding-right: 80px;"/>
+
+    <label style="display: block; padding-top: 10px;">Alamat</label>
+    <input name="alamat" placeholder="Alamat" style="display: flex; padding-right: 80px;" />
+
+    <label style="display: block; padding-top: 10px;">Masukan Foto profil</label>
+    <input type="file" style="background: aquamarine;" />
+    <button type="submit" style="display: flex; background-color: blue; justify-content: center; padding-left: 115px; padding-right: 115px; margin-top: 20px; margin-bottom: 20px;">Create</button>
 </form>
