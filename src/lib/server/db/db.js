04fb79105e475e13293c10d74be5799812f51532
Mamadz07 <muhammadilham3025@gmail.com>
2026-05-05 20:46:55 +0700

    sedikit-perubahan

--- src/lib/server/db/db.js
+++ src/lib/server/db/db.js
@@ -1,10 +1,11 @@
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 import * as schema from './schema';
+import {env} from '$env/dynamic/private'
 
 const client = createClient({
-    url: process.env.TURSO_DATABASE_URL,
-    authToken: process.env.TURSO_AUTH_TOKEN
+    url: env.DATABASE_URL,
+    authToken: env.DATABASE_AUTH_TOKEN || ''
 });
 
 export const db = drizzle(client, {schema});
